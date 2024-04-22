38b9b89426e584182bcd6f46d7f6c6c60da1b3db
Greg Hunt <freshbrewedweb@gmail.com>
2024-04-22 09:12:33 -0400

    Dynamic conversation

--- client/Providers/ChatProvider.tsx
+++ client/Providers/ChatProvider.tsx
@@ -1,4 +1,4 @@
-import React, { useState, createContext, useContext } from 'react';
+import React, { useState, createContext, useContext, useEffect } from 'react';
 import { useClientSettings } from './ClientSettingsProvider';
 import axios from 'axios';
 import { useScreen } from './ScreenProvider';
@@ -36,31 +36,20 @@ export default function ChatProvider({
 }: {
   children: React.ReactNode;
 }) {
+  const siteId = '9bd7360f-6aeb-4204-b1a6-624d004701a3';
+  const wp_user_id = 1;
+  const token = `eyJpdiI6InYrRXJBOHdWd0ovVGpabXBpRUdhaVE9PSIsInZhbHVlIjoiQnFLWC9ka0xCNHEycWZBbjN1MzQ5SkZxVmZic2UxTEtwQzdGQTk2aWIvMTVoU3N1b0RmVE13TDkvd3JNYmNTV1VYWXY2bnF1WU9mTEo0VkdnT2tmUmc9PSIsIm1hYyI6ImIzZGUwMWYyNzRmZGZjYTMzZjZjNjRiMzI0OTVlNTM5Y2RlM2Q4ZGY0ZWEwNTJhN2YxZjQxZGRlZTc3OTdlNmIiLCJ0YWciOiIifQ==`;
+
   const screen = useScreen();
   const { settings, setSettings } = useClientSettings();
   const [open, setOpen] = useState(settings.chatOpen ?? false);
-  const [conversation, setConversation] = useState<Message[]>([
-    {
-      id: '1',
-      role: 'agent',
-      message: 'Hello! How can I help you today?',
-    },
-    {
-      id: '2',
-      role: 'user',
-      message: 'I need help with my order.',
-    },
-    {
-      id: '3',
-      role: 'agent',
-      message: 'Sure! Can you provide me with your order number?',
-    },
-    {
-      id: '4',
-      role: 'user',
-      message: 'Yes, it is 123456.',
-    },
-  ]);
+  const [conversation, setConversation] = useState<Message[]>([]);
+
+  useEffect(() => {
+    if (open) {
+      getConversation();
+    }
+  }, [open]);
 
   function toggle() {
     const newVal = !open;
@@ -68,15 +57,47 @@ export default function ChatProvider({
     setSettings({ chatOpen: newVal });
   }
 
+  async function getConversation() {
+    const response = await axios.get(`http://localhost/api/sites/${siteId}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    response.data.forEach((userRequest: any) => {
+      let messages: Message[] = [];
+
+      messages = [
+        ...messages,
+        {
+          id: userRequest.id,
+          role: 'user',
+          message: userRequest.message,
+        },
+      ];
+
+      if (userRequest.agent_actions) {
+        const agentMessages: Message[] = userRequest.agent_actions.map(
+          (agentAction: any) => {
+            return {
+              id: agentAction.id,
+              role: 'agent',
+              message: JSON.parse(agentAction.action).text,
+            };
+          },
+        );
+        messages = [...messages, ...agentMessages];
+      }
+
+      setConversation((prev) => [...prev, ...messages]);
+    });
+  }
+
   async function userRequest(
     message: string,
   ): Promise<CreateUserRequestResponse> {
-    const siteId = '9bd7360f-6aeb-4204-b1a6-624d004701a3';
-    const wp_user_id = 1;
-    const token = `eyJpdiI6InYrRXJBOHdWd0ovVGpabXBpRUdhaVE9PSIsInZhbHVlIjoiQnFLWC9ka0xCNHEycWZBbjN1MzQ5SkZxVmZic2UxTEtwQzdGQTk2aWIvMTVoU3N1b0RmVE13TDkvd3JNYmNTV1VYWXY2bnF1WU9mTEo0VkdnT2tmUmc9PSIsIm1hYyI6ImIzZGUwMWYyNzRmZGZjYTMzZjZjNjRiMzI0OTVlNTM5Y2RlM2Q4ZGY0ZWEwNTJhN2YxZjQxZGRlZTc3OTdlNmIiLCJ0YWciOiIifQ==`;
-
     const response = await axios.post(
-      `http://localhost/api/sites/${siteId}/request`,
+      `http://localhost/api/sites/${siteId}`,
       {
         message,
         wp_user_id,
