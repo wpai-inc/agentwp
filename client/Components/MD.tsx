84ef67ce7756d034b15c04d7376c2eb5b1526e0c
Greg Hunt <freshbrewedweb@gmail.com>
2024-05-09 17:17:53 -0400

    Style markdown in responses

--- client/Components/MD.tsx
+++ client/Components/MD.tsx
@@ -2,5 +2,9 @@ import Markdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 
 export default function MD({ children }: { children: string }) {
-  return <Markdown remarkPlugins={[remarkGfm]}>{children}</Markdown>;
+  return (
+    <div className="prose">
+      <Markdown remarkPlugins={[remarkGfm]}>{children}</Markdown>
+    </div>
+  );
 }
