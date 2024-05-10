acd05aa3249bd62c1519621d4c215bf9158dd86b
Anthony Gainor <anthony@againor.com>
2024-05-09 21:54:15 -0300

    ENG-1321: Add dragging functionality and memorize last known position

--- client/Components/ConvoContainer.tsx
+++ client/Components/ConvoContainer.tsx
@@ -1,15 +1,26 @@
+import { useEffect, useRef } from "react";
 import { cn } from '@/lib/utils';
 import { useChat } from '@/Providers/ChatProvider';
 import Dialog from './Dialog';
 import MessageBox from './MessageBox';
 import ChatTopBar from '@/Page/Admin/Chat/Partials/ChatTopBar';
 import WindowActions from '@/Page/Admin/Chat/Partials/WindowActions';
+import { useClientSettings } from '@/Providers/ClientSettingsProvider';
 
 export default function ConvoContainer() {
+  const windowRef = useRef(null);
   const { open } = useChat();
+  const { settings } = useClientSettings();
+
+  useEffect(() => {
+    const windowElement = windowRef.current;
+    windowElement.style.left = settings.x;
+    windowElement.style.top = settings.y;
+  }, []);
 
   return (
     <div
+      ref={windowRef}
       className={cn(
         'transition fixed bottom-4 right-10',
         'h-[90vh] w-[500px] z-[1000] bg-brand-gray',
--- client/Page/Admin/Chat/Partials/ChatTopBar.tsx
+++ client/Page/Admin/Chat/Partials/ChatTopBar.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { cn } from '@/lib/utils';
 import SettingsIcons from '@material-design-icons/svg/filled/settings.svg?react';
 import AccountIcon from '@material-design-icons/svg/outlined/account_circle.svg?react';
 import { Button } from '@/Components/ui/button';
 import { Badge } from '@/Components/ui/badge';
+import { useClientSettings } from '@/Providers/ClientSettingsProvider';
 
 export default function ChatTopBar() {
+  const { setSettings } = useClientSettings();
+  const topBarRef = useRef(null);
+
   function onUpgradeClick(e: React.FormEvent) {
     e.preventDefault();
   }
@@ -18,11 +22,47 @@ export default function ChatTopBar() {
     e.preventDefault();
   }
 
+  function startDrag(e) {
+    let currentPosX = e.clientX - e.target.getBoundingClientRect().left;
+    let currentPosY = e.clientY - e.target.getBoundingClientRect().top;
+
+    const drag = (target, x, y) => {
+      target.style.left = x - currentPosX + 'px';
+      target.style.top = y - currentPosY + 'px';
+    }
+
+    const handleDrag = (e) => {
+      drag(e.target.parentNode, e.pageX, e.pageY);
+    }
+
+    drag(e.target.parentNode, e.pageX, e.pageY);
+
+    e.target.addEventListener('mousemove', handleDrag);
+    e.target.addEventListener('mouseup', () => {
+      setSettings({
+        x: e.target.parentNode.style.left,
+        y: e.target.parentNode.style.top,
+      });
+      e.target.removeEventListener('mousemove', handleDrag);
+      e.target.onmouseup = null;
+    });
+  }
+
+  useEffect(() => {
+    const element = topBarRef.current;
+    element.addEventListener('mousedown', startDrag);
+
+    return () => {
+      element.removeEventListener('mousedown', startDrag);
+    }
+  }, []);
+
   return (
     <div
+      ref={topBarRef}
       className={cn(
         'py-2 px-2 border-b border-b-brand-gray-25',
-        'flex justify-between'
+        'flex justify-between cursor-move'
       )}
     >
       <div className="flex items-center gap-2">
