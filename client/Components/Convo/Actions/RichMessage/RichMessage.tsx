d637a42a655800161826f69f700d0a907d9ceb59
Anthony Gainor <anthony@againor.com>
2024-05-15 17:33:59 -0300

    Add GraphDataPoint Type and improve typing for Rich Messages

--- client/Components/Convo/Actions/RichMessage/RichMessage.tsx
+++ client/Components/Convo/Actions/RichMessage/RichMessage.tsx
@@ -4,65 +4,75 @@ import Pie from '@/Components/Charts/Pie';
 import NavigatableButton from '@/Components/NavigatableButton';
 import ActionSimpleMessage from '@/Components/Convo/Actions/ActionSimpleMessage';
 import { AgentAction } from '@/Providers/UserRequestsProvider';
+import { MessageAction } from '@wpai/schemas';
+
+type GraphDataPoint = {
+  label: string;
+  value: string;
+}
 
 const RichMessage = (props: AgentAction) => {
-  const isGraph = !!props.action.graph;
-  const areButtons = !!props.action.buttons;
 
-  if (isGraph) {
-    const graphType = props.action.graph.graphType;
-    const data = props.action.graph.data;
-    if (graphType === 'bar') {
+  if (props.action.ability === 'message') {
+    const isGraph = !!props.action.graph;
+    const areButtons = !!props.action.buttons;
+
+    const action = props.action as MessageAction;
+    if (isGraph) {
+      const graphType = action.graph?.graphType;
+      const data = action.graph?.data;
+      if (graphType === 'bar') {
+        return (
+          <div className="flex flex-col gap-4">
+            <ActionSimpleMessage {...props} />
+            <Bar
+              data={data ?? []}
+              valueDataKey="value"
+              xDataKey="label"
+              width={'100%'}
+            />
+          </div>
+        );
+      } else if (graphType === 'line') {
+        return (
+          <div className="flex flex-col gap-4">
+            <ActionSimpleMessage {...props} />
+            <Line
+              data={data ?? []}
+              valueDataKey="value"
+              xDataKey="label"
+              width={'100%'}
+            />
+          </div>
+        );
+      } else if (graphType === 'pie') {
+        return (
+          <div className="flex flex-col gap-4">
+            <ActionSimpleMessage {...props} />
+            <Pie
+              data={data ?? []}
+              valueDataKey="value"
+              dataKey="label"
+              width={'100%'}
+            />
+          </div>
+        );
+      }
+    } else if (areButtons) {
+      const buttons = action.buttons as any[];
       return (
-        <div className="flex flex-col gap-4">
+        <>
           <ActionSimpleMessage {...props} />
-          <Bar
-            data={data}
-            valueDataKey="value"
-            xDataKey="label"
-            width={'100%'}
-          />
-        </div>
-      );
-    } else if (graphType === 'line') {
-      return (
-        <div className="flex flex-col gap-4">
-          <ActionSimpleMessage {...props} />
-          <Line
-            data={data}
-            valueDataKey="value"
-            xDataKey="label"
-            width={'100%'}
-          />
-        </div>
-      );
-    } else if (graphType === 'pie') {
-      return (
-        <div className="flex flex-col gap-4">
-          <ActionSimpleMessage {...props} />
-          <Pie
-            data={data}
-            valueDataKey="value"
-            dataKey="label"
-            width={'100%'}
-          />
-        </div>
-      );
-    }
-  } else if (areButtons) {
-    const buttons = props.action.buttons as any[];
-    return (
-      <>
-        <ActionSimpleMessage {...props} />
-        {buttons.length > 0
-          ? buttons.map((b) => (
+          {buttons.length > 0
+            ? buttons.map((b) => (
               <NavigatableButton text={b.label} link={b.url} className={''} />
             ))
-          : null}
-      </>
-    );
-  } else {
-    return <ActionSimpleMessage {...props} />;
+            : null}
+        </>
+      );
+    } else {
+      return <ActionSimpleMessage {...props} />;
+    }
   }
 
   return (
