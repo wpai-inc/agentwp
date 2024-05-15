19c3765adae591816db4f4d2614e1421ca1db7f2
Ovidiu Galatan <galatanovidiu@gmail.com>
2024-05-15 17:47:58 +0300

    Design improvements

--- client/Components/BrandIcon.tsx
+++
@@ -1,7 +0,0 @@
-export default function BrandIcon() {
-    return (
-        <div className="text-center">
-            ICON
-        </div>
-    );
-}
--- client/Page/Admin/Settings/Wizard/UserAccess.tsx
+++ client/Page/Admin/Settings/Wizard/UserAccess.tsx
@@ -1,23 +1,18 @@
-import { useEffect, useState } from "react";
-import adminRequest from "@/lib/adminRequest";
-import type { agentwpUser } from "@/Types/types";
+import { Logo } from "@/Components/Logo";
 import { Button } from "@/Components/ui/button";
-import { User } from "@/Page/Admin/Settings/Partials/User";
-import SearchUser from "@/Page/Admin/Settings/Partials/SearchUser";
-import BrandIcon from "@/Components/BrandIcon";
 import UsersManagement from "@/Page/Admin/Settings/SubPages/UsersManagement";
 
 export default function UserAccess({ onGoToAboutPage }: { onGoToAboutPage: () => void }) {
     return (
-            <div className="max-w-[470px] mx-auto text-base mt-12">
-                <BrandIcon />
-                <div className="text-4xl font-bold text-center ">
-                    Choose Who Can Use AgentWP
-                </div>
-                <UsersManagement />
-                <div className={"text-center mt-16"}>
-                    <Button type="button" onClick={() => onGoToAboutPage()}>Continue</Button>
-                </div>
+        <div className="max-w-[470px] mx-auto text-base mt-12">
+            <Logo className="mx-auto" />
+            <div className="text-4xl font-bold text-center mt-4">
+                Choose Who Can Use AgentWP
             </div>
+            <UsersManagement />
+            <div className={"text-center mt-16"}>
+                <Button variant="brand" className="px-20" onClick={() => onGoToAboutPage()}>Continue</Button>
+            </div>
+        </div>
     );
 }
