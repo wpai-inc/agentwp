a289efc333cfe3ba17788788159b5523b26c84d3
Greg Hunt <freshbrewedweb@gmail.com>
2024-05-22 09:02:16 -0400

    Fixed page loading

--- client/Components/Chat/Convo/Message/Message.tsx
+++ client/Components/Chat/Convo/Message/Message.tsx
@@ -4,20 +4,19 @@ import UserRequest from './UserRequest';
 import type { UserRequestType } from '@/Providers/UserRequestsProvider';
 import { useUserRequests } from '@/Providers/UserRequestsProvider';
 
-export default function Message(userRequest: UserRequestType) {
-  const page = usePage();
+export default function Message( userRequest: UserRequestType ) {
+  const { page } = usePage();
   const { currentAction, currentUserRequestId } = useUserRequests();
-  const pending =
-    !currentAction?.final && userRequest.id === currentUserRequestId;
+  const pending = ! currentAction?.final && userRequest.id === currentUserRequestId;
 
   return (
-    <div id={userRequest.id} className="border-b border-brand-gray-25 mb-4">
-      <UserRequest userRequest={userRequest} user={page.user} />
+    <div id={ userRequest.id } className="border-b border-brand-gray-25 mb-4">
+      <UserRequest userRequest={ userRequest } user={ page.user } />
       <AgentResponse
-        userRequestId={userRequest.id}
-        time={userRequest.human_created_at}
-        agentActions={userRequest.agent_actions}
-        pending={pending}
+        userRequestId={ userRequest.id }
+        time={ userRequest.human_created_at }
+        agentActions={ userRequest.agent_actions }
+        pending={ pending }
       />
     </div>
   );
