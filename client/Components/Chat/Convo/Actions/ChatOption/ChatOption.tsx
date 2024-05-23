b16deb0f2786ff19d794e540e78916f3a10dee45
Anthony Gainor <anthony@againor.com>
2024-05-23 08:50:37 -0300

    Add Chatoption components
    Reduce the shadow when the container is maximized

---
+++ client/Components/Chat/Convo/Actions/ChatOption/ChatOption.tsx
@@ -0,0 +1,29 @@
+import { cn } from "@/lib/utils";
+
+type Props = {
+  message: string;
+  onClick: () => void;
+}
+
+const ChatOption = ({
+  message,
+  onClick,
+}: Props) => {
+  return (
+    <div
+      onClick={onClick}
+      className={cn(
+        'flex items-center justify-center shadow-sm',
+        'p-4 bg-brand-gray-25 cursor-pointer h-full w-full',
+        'rounded-lg hover:bg-gray-50',
+        'text-center text-slate-500 transition duration-200'
+      )}
+    >
+      <p className=" ">
+        {message}
+      </p>
+    </div>
+  )
+};
+
+export default ChatOption;
