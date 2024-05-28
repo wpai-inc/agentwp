f5dd06ba69489ce28bbce55d31929c324c08065f
Anthony Gainor <anthony@againor.com>
2024-05-28 15:25:47 -0300

    Create a loading component to show when conversation is loading

--- client/Components/Chat/Convo/Dialog.tsx
+++ client/Components/Chat/Convo/Dialog.tsx
@@ -1,9 +1,11 @@
+import React, { useState } from 'react';
 import Message from '@/Components/Chat/Convo/Message/Message';
 import { UserRequestType } from '@/Providers/UserRequestsProvider';
 import ChatWelcome from '@/Components/ChatWelcome';
 import { useError } from '@/Providers/ErrorProvider';
 import { ChatError } from '@/Components/Chat/Alerts/Error';
 import { cn } from '@/lib/utils';
+import LoadingScreen from '@/Components/Chat/LoadingScreen';
 
 export default function Dialog({
   conversation,
@@ -11,16 +13,24 @@ export default function Dialog({
   conversation: UserRequestType[];
 }) {
   const { errors } = useError();
+  const [loading, setLoading] = useState(false);
   return (
     <div className={cn(
       'flex-1 flex flex-col-reverse overflow-y-auto p-4 relative'
     )}>
-      {!conversation.length && (
-        <ChatWelcome />
+      {loading ? (
+        <LoadingScreen />
+      ): (
+        <>
+          {!conversation.length && (
+            <ChatWelcome />
+          )}
+          {conversation.map((msg) => (
+            <Message key={msg.id} {...msg} />
+          ))}
+        </>
       )}
-      {conversation.map((msg) => (
-        <Message key={msg.id} {...msg} />
-      ))}
+
       {!!errors.length && <ChatError errors={errors} />}
     </div>
   );
