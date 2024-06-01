2456b47a25451918957451ef16c545effd345e2e
Greg Hunt <freshbrewedweb@gmail.com>
2024-06-01 13:21:04 -0400

    Center screen on small maximized

--- client/Components/Chat/Convo/Dialog.tsx
+++ client/Components/Chat/Convo/Dialog.tsx
@@ -5,23 +5,18 @@ import { useError } from '@/Providers/ErrorProvider';
 import { ChatError } from '@/Components/Chat/Alerts/Error';
 import { cn } from '@/lib/utils';
 
-export default function Dialog({
-  conversation,
-}: {
-  conversation: UserRequestType[];
-}) {
+export default function Dialog( { conversation }: { conversation: UserRequestType[] } ) {
   const { errors } = useError();
   return (
-    <div className={cn(
-      'flex-1 flex flex-col-reverse overflow-y-auto p-4 relative'
-    )}>
-      {!conversation.length && (
-        <ChatWelcome />
-      )}
-      {conversation.map((msg) => (
-        <Message key={msg.id} {...msg} />
-      ))}
-      {!!errors.length && <ChatError errors={errors} />}
+    <div
+      className={ cn(
+        'flex-1 flex flex-col-reverse overflow-y-auto p-4 relative max-w-screen-md mx-auto',
+      ) }>
+      { ! conversation.length && <ChatWelcome /> }
+      { conversation.map( msg => (
+        <Message key={ msg.id } { ...msg } />
+      ) ) }
+      { !! errors.length && <ChatError errors={ errors } /> }
     </div>
   );
 }
