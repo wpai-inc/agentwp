f3fdf65af99731c5c0f15bfb4c3ecdd0e194ff7f
Ovidiu Galatan <galatanovidiu@gmail.com>
2024-06-06 17:19:08 +0300

    Refactor WriteToEditor service for improved code readability and maintainability

--- client/Services/WriteToEditor.ts
+++ client/Services/WriteToEditor.ts
@@ -2,9 +2,9 @@ declare const wp: any;
 
 export function WriteToEditor( { content }: { content: string } ) {
   console.log( 'WRITE TO EDITOR', content );
-  //   const { select } = wp.data;
-  //   const { dispatch } = wp.data.dispatch( 'core/editor' );
-  // const postContent = select( 'core/editor' ).getEditedPostContent();
-  //   const newContent = content; // postContent + content;
-  //   dispatch( 'core/editor' ).editPost( { content: newContent } );
+  const { select } = wp.data;
+  const { dispatch } = wp.data.dispatch( 'core/editor' );
+  const postContent = select( 'core/editor' ).getEditedPostContent();
+  const newContent = content; // postContent + content;
+  dispatch( 'core/editor' ).editPost( { content: newContent } );
 }
