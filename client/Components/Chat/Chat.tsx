7c6b4d5447099a3b16690182a6686f998f48e6c8
Ovidiu Galatan <galatanovidiu@gmail.com>
2024-07-03 19:54:38 +0300

    chore: Update Chat component to conditionally render based on page state
    The Chat component has been updated to conditionally render the ChatContainer and ChatTrigger components based on the state of the page. This change ensures that the Chat component is only displayed when the onboarding is completed and the user has access to the agentwp.

--- client/Components/Chat/Chat.tsx
+++ client/Components/Chat/Chat.tsx
@@ -1,13 +1,20 @@
 import ChatContainer from '@/Components/Chat/ChatContainer';
 import ChatTrigger from '@/Components/Chat/ChatTrigger';
 import { useChat } from '@/Providers/ChatProvider';
+import { usePage } from '@/Providers/PageProvider';
 
 export default function Chat() {
   const { open, expanding } = useChat();
+  const { page } = usePage();
+
   return (
     <>
-      { ( open || expanding ) && <ChatContainer /> }
-      <ChatTrigger open={ open } />
+      { page.onboarding_completed && page.agentwp_access && (
+        <>
+          { ( open || expanding ) && <ChatContainer /> }
+          <ChatTrigger open={ open } />
+        </>
+      ) }
     </>
   );
 }
