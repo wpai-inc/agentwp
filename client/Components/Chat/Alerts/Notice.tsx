04abd189e0042b92ac2274b7e538be13403f917b
Greg Hunt <freshbrewedweb@gmail.com>
2024-07-11 17:31:59 -0400

    small UI change

--- client/Components/Chat/Alerts/Notice.tsx
+++ client/Components/Chat/Alerts/Notice.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 export function ChatNotice( { children }: { children: React.ReactNode } ) {
   return (
     <div className="absolute bottom-0 right-0 left-0 z-20 px-2">
-      <Alert variant="destructive" className={ cn( 'w-full bg-[#edacd2] text-pink-900 py-2' ) }>
+      <Alert variant="informative" className={ cn( 'w-full py-2' ) }>
         <AlertDescription>{ children }</AlertDescription>
       </Alert>
     </div>
