542ee7864ac502b7fbd9c90ecc8b7e703bfcbb80
Ovidiu Galatan <galatanovidiu@gmail.com>
2024-07-12 15:59:12 +0300

    bugfix: wp?.data?.select('core/editor') ca =n be null

--- client/lib/isEditorReady.ts
+++ client/lib/isEditorReady.ts
@@ -1,7 +1,7 @@
 declare const wp: any;
 export default function isEditorReady(callback: () => void) {
   const closeListener = wp?.data?.subscribe(() => {
-    const isReady = wp?.data?.select('core/editor').__unstableIsEditorReady();
+    const isReady = wp?.data?.select('core/editor')?.__unstableIsEditorReady();
     if (!isReady) {
       return;
     }
