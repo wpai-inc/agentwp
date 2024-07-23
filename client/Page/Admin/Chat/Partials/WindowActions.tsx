472193aeb5f9dc7629ecd0fcd97f2e88408fc110
Greg Hunt <freshbrewedweb@gmail.com>
2024-07-23 19:10:53 -0400

    Cleaned up window actions and separated drag handle

--- client/Page/Admin/Chat/Partials/WindowActions.tsx
+++ client/Page/Admin/Chat/Partials/WindowActions.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import CloseIcon from '@material-design-icons/svg/outlined/close.svg?react';
 import MaximizeIcon from '@material-design-icons/svg/outlined/open_with.svg?react';
 import DragIcon from '@material-design-icons/svg/outlined/drag_indicator.svg?react';
@@ -10,8 +9,8 @@ import { AgentTooltip } from '@/Components/ui/tooltip';
 const Corners = () => {
   return (
     <>
-      <div className="absolute -top-3 left-3 border-brand-gray min-h-4 w-4 rounded-br-full border-b-4 border-r-4 shadow-xl"></div>
-      <div className="absolute -bottom-3 left-3 border-brand-gray min-h-4 w-4 rounded-tr-full border-t-4 border-r-4 shadow-xl"></div>
+      <div className="absolute -top-3 -right-1 border-brand-gray h-4 w-4 rounded-br-full border-b-4 border-r-4 shadow-xl"></div>
+      <div className="absolute -bottom-3 -right-1 border-brand-gray h-4 w-4 rounded-tr-full border-t-4 border-r-4 shadow-xl"></div>
     </>
   );
 };
@@ -32,54 +31,59 @@ export default function WindowActions( {
   handleDrag: ( e: MouseEvent ) => void;
 } ) {
   const { toggle } = useChat();
-  const [ isFirstLoad ] = useState( false );
 
   return (
     <div
       onMouseDown={ e => handleDrag( e.nativeEvent ) }
       className={ cn(
-        'absolute bg-brand-gray h-20 w-6 top-16 left-0 flex flex-col items-center justify-center gap-2 rounded-bl-lg rounded-tl-lg opacity-0 transition-all duration-300 shadow-xl',
+        'absolute top-0 left-0 opacity-0 transition-all duration-300 h-full grid grid-rows-2 overflow-hidden pl-2',
         {
           'opacity-100 -translate-x-full': show,
-          'animate-slide-in': show,
-          'animate-slide-out': ! show && ! isFirstLoad,
         },
       ) }
       { ...props }>
-      <AgentTooltip content="Drag or double-click to reset window position" side="right">
-        <DragIcon
-          id="dragHandle"
-          className={ cn( 'handle h-6 w-6 hover:text-amber-500 cursor-move', 'text-gray-400' ) }
-        />
-      </AgentTooltip>
-      { isMaximized ? (
-        <AgentTooltip content="Return chat window to normal size" side="right">
-          <ReduceWindowIcon
-            onClick={ restoreWindow }
-            className={ cn( 'h-4 w-4 cursor-pointer hover:text-teal-500', 'text-gray-400' ) }
-          />
-        </AgentTooltip>
-      ) : (
-        <AgentTooltip
-          content="Maximize the chat window to take all the available space"
-          side="right">
-          <MaximizeIcon
-            onClick={ maximizeWindow }
-            className={ cn(
-              'h-4 w-4 cursor-pointer hover:text-teal-500 rotate-45',
-              'text-gray-400',
-            ) }
-          />
-        </AgentTooltip>
-      ) }
+      <div>
+        <div className="bg-brand-gray w-6 flex flex-col items-center justify-center gap-2 rounded-bl-lg rounded-tl-lg shadow-xl relative z-0 mt-16 py-2">
+          { isMaximized ? (
+            <AgentTooltip content="Return chat window to normal size" side="right">
+              <ReduceWindowIcon
+                onClick={ restoreWindow }
+                className={ cn( 'h-4 w-4 cursor-pointer hover:text-teal-500', 'text-gray-400' ) }
+              />
+            </AgentTooltip>
+          ) : (
+            <AgentTooltip
+              content="Maximize the chat window to take all the available space"
+              side="right">
+              <MaximizeIcon
+                onClick={ maximizeWindow }
+                className={ cn(
+                  'h-4 w-4 cursor-pointer hover:text-teal-500 rotate-45',
+                  'text-gray-400',
+                ) }
+              />
+            </AgentTooltip>
+          ) }
 
-      <AgentTooltip content="Close window" side="right">
-        <CloseIcon
-          onClick={ toggle }
-          className={ cn( 'h-5 w-5 cursor-pointer hover:text-red-500', 'text-gray-400' ) }
-        />
-      </AgentTooltip>
-      <Corners />
+          <AgentTooltip content="Close window" side="right">
+            <CloseIcon
+              onClick={ toggle }
+              className={ cn( 'h-4 w-4 cursor-pointer hover:text-red-500', 'text-gray-400' ) }
+            />
+          </AgentTooltip>
+          <Corners />
+        </div>
+      </div>
+      <div>
+        <AgentTooltip content="Drag or double-click to reset window position" side="right">
+          <button
+            id="dragHandle"
+            className="handle hover:text-amber-500 cursor-move text-gray-400 -translate-y-full">
+            <DragIcon />
+            <DragIcon className="-mt-1.5" />
+          </button>
+        </AgentTooltip>
+      </div>
     </div>
   );
 }
