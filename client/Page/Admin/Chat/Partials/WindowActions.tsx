b76911d84a0fe7f88a5299ddba249991f7981a20
Greg Hunt <freshbrewedweb@gmail.com>
2024-07-23 18:21:08 -0400

    shadow on window actions

--- client/Page/Admin/Chat/Partials/WindowActions.tsx
+++ client/Page/Admin/Chat/Partials/WindowActions.tsx
@@ -10,16 +10,8 @@ import { AgentTooltip } from '@/Components/ui/tooltip';
 const Corners = () => {
   return (
     <>
-      <div
-        className={ cn(
-          'absolute -top-3 left-3 border-brand-gray',
-          'min-h-4 w-4 rounded-br-full border-b-4 border-r-4',
-        ) }></div>
-      <div
-        className={ cn(
-          'absolute -bottom-3 left-3 border-brand-gray',
-          'min-h-4 w-4 rounded-tr-full border-t-4 border-r-4',
-        ) }></div>
+      <div className="absolute -top-3 left-3 border-brand-gray min-h-4 w-4 rounded-br-full border-b-4 border-r-4 shadow-xl"></div>
+      <div className="absolute -bottom-3 left-3 border-brand-gray min-h-4 w-4 rounded-tr-full border-t-4 border-r-4 shadow-xl"></div>
     </>
   );
 };
@@ -46,9 +38,7 @@ export default function WindowActions( {
     <div
       onMouseDown={ e => handleDrag( e.nativeEvent ) }
       className={ cn(
-        'absolute bg-brand-gray h-20 w-6 top-16 left-0',
-        'flex flex-col items-center justify-center gap-2',
-        'rounded-bl-lg rounded-tl-lg opacity-0 transition-all duration-300',
+        'absolute bg-brand-gray h-20 w-6 top-16 left-0 flex flex-col items-center justify-center gap-2 rounded-bl-lg rounded-tl-lg opacity-0 transition-all duration-300 shadow-xl',
         {
           'opacity-100 -translate-x-full': show,
           'animate-slide-in': show,
