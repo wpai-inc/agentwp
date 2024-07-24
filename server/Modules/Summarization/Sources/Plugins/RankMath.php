4b32eba185457f82167e302799d223d745750866
Sergi Khizanishvili <sergi.khizanishvili@hotmail.com>
2024-07-24 20:07:22 +0400

    Added several checks to rank math

--- server/Modules/Summarization/Sources/Plugins/RankMath.php
+++ server/Modules/Summarization/Sources/Plugins/RankMath.php
@@ -14,12 +14,20 @@ class RankMath implements SourceInterface
     public function isActive(): bool
     {
         return (
-            is_plugin_active('seo-by-rank-math/rank-math.php') || is_plugin_active('seo-by-rank-math-pro/rank-math-pro.php')
+            is_plugin_active('seo-by-rank-math/rank-math.php') || is_plugin_active('seo-by-rank-math /rank-math.php') || is_plugin_active('seo-by-rank-math-pro/rank-math-pro.php')
         ) && function_exists('rank_math');
     }
 
     public function getData(): array
     {
+        if ( ! class_exists( '\RankMath\Rest\Headless' ) || ! method_exists( '\RankMath\Rest\Headless', 'get_head' ) ) {
+            return [];
+        }
+
+        if ( ! rank_math()->variables || ! method_exists( rank_math()->variables, 'setup' ) ) {
+            return [];
+        }
+
         $data = ( new \RankMath\Rest\Headless )->get_head( new \WP_REST_Request() );
         if ( is_wp_error( $data ) || empty( $data ) || ! isset( $data->data ) || empty( $data->data ) ) {
             return [];
