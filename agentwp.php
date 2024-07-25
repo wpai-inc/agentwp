a185609e8138ea68556c82c9616940dc506dbf05
Greg Hunt <freshbrewedweb@gmail.com>
2024-07-25 10:48:44 -0400

    Updated version to 0.1.1

--- agentwp.php
+++ agentwp.php
@@ -4,7 +4,7 @@
  * Plugin Name: AgentWP
  * Plugin URI: https://codewp.ai
  * Description: Adds a AI Agent to your WordPress installation.
- * Version: 0.1.0
+ * Version: 0.1.1
  * Author: WP AI Inc.
  * Author URI: https://codewp.ai
  * License: GPL2
