c34499ef70884d339ced86f6c1f9c4c5de6b0d1d
Greg Hunt <freshbrewedweb@gmail.com>
2024-08-03 13:17:24 -0400

    Larger handle drag area, smaller hover effect

--- client/Components/Chat/ResizeHandles/Handle.tsx
+++ client/Components/Chat/ResizeHandles/Handle.tsx
@@ -10,12 +10,20 @@ export default function Handle( {
 } ) {
   return (
     <div
-      className={ cn( 'absolute z-10 hover:bg-brand-primary transition overflow-hidden', {
-        'top-0 left-5 right-5 cursor-ns-resize h-1': position === 'top',
-        'bottom-0 left-5 right-5 cursor-ns-resize h-1': position === 'bottom',
-        'top-5 bottom-5 left-0 cursor-ew-resize w-1': position === 'left',
-        'top-5  bottom-5 right-0 cursor-ew-resize w-1': position === 'right',
+      className={ cn( 'absolute z-10 group overflow-hidden', {
+        'top-0 left-5 right-5 cursor-ns-resize h-1.5': position === 'top',
+        'bottom-0 left-5 right-5 cursor-ns-resize h-1.5': position === 'bottom',
+        'top-5 bottom-5 left-0 cursor-ew-resize w-1.5': position === 'left',
+        'top-5  bottom-5 right-0 cursor-ew-resize w-1.5': position === 'right',
       } ) }
-      onMouseDown={ e => resizeHandler( e.nativeEvent, position ) }></div>
+      onMouseDown={ e => resizeHandler( e.nativeEvent, position ) }>
+      <div
+        className={ cn( 'group-hover:bg-brand-primary transition', {
+          'mt-0.5 h-1 w-full': position === 'bottom',
+          'mb-0.5 h-1 w-full': position === 'top',
+          'ml-0.5 w-1 h-full': position === 'right',
+          'mr-0.5 w-1 h-full': position === 'left',
+        } ) }></div>
+    </div>
   );
 }
