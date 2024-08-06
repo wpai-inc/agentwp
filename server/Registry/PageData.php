a43bd30bef477751806f8496d261b52478343fd3
Ovidiu Galatan <galatanovidiu@gmail.com>
2024-08-06 23:10:24 +0300

    Fix retrieval of general settings in PageData class

--- server/Registry/PageData.php
+++ server/Registry/PageData.php
@@ -58,7 +58,7 @@ class PageData implements Registrable
             'user' => $current_user,
             'onboarding_completed' => $this->main->settings->onboarding_completed,
             'account_settings' => $this->main->accountSettings()->get(),
-            'general_settings' => $this->settings->getGeneralSettings(),
+            'general_settings' => $this->main->settings->getGeneralSettings(),
         ];
         ?>
         <script>
