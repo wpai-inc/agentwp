0614956840e95496178f922049e957ea8f7f7dc6
Ovidiu Galatan <galatanovidiu@gmail.com>
2024-08-08 15:55:56 +0300

    Add className prop to Spinner component

--- client/Components/Spinner.tsx
+++ client/Components/Spinner.tsx
@@ -1,9 +1,9 @@
 import { cn } from '@/lib/utils.js';
 
-export function Spinner( { show }: { show: boolean } ) {
+export function Spinner( { show, className }: { show: boolean; className?: string } ) {
   return (
     <svg
-      className={ cn( 'animate-spin -ml-1 mr-3 h-5 w-5 text-gray-400 hidden', {
+      className={ cn( 'animate-spin h-5 w-5 text-gray-400 hidden', className, {
         'inline-block': show,
       } ) }
       xmlns="http://www.w3.org/2000/svg"
@@ -17,7 +17,6 @@ export function Spinner( { show }: { show: boolean } ) {
         stroke="currentColor"
         strokeWidth="4"></circle>
       <path
-        className="opacity-75"
         fill="currentColor"
         d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
     </svg>
