307cad4446d8e2bd6f3c921462c67d5ee70ae2fa
Ovidiu Galatan <galatanovidiu@gmail.com>
2024-08-08 22:32:06 +0300

    Remove debug logging and unused timing code

--- server/Controllers/SiteDataController.php
+++ server/Controllers/SiteDataController.php
@@ -18,21 +18,15 @@ class SiteDataController extends BaseController
             $this->error('You do not have permission to perform this action');
         }
 
-        $ts = microtime(true);
         $summarizer = new SiteSummarizer();
-        error_log('SiteSummarizer: '.(microtime(true) - $ts));
-//        if ($summarizer->hasUpdated()) {
+        if ($summarizer->hasUpdated()) {
             $this->main->client(false)->summarizeSite(json_encode($summarizer->getData()));
-//        }
+        }
 
-        $ts2 = microtime(true);
         $site_data = new SiteData();
-        error_log('SiteData: '.(microtime(true) - $ts2));
-//        if ($site_data->hasUpdated()) {
+        if ($site_data->hasUpdated()) {
             $this->main->client(false)->indexSite(json_encode($site_data->getData()));
-//        }
-
-        error_log('Total: '.(microtime(true) - $ts));
+        }
         $this->respond([]);
     }
 }
