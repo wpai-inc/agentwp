4f100369f0ad4ba40d9a09446a062a4fe437f327
Ovidiu Galatan <galatanovidiu@gmail.com>
2024-08-15 21:27:41 +0300

    ENG-1584: Update database import process

--- client/Providers/InputSelectProvider.tsx
+++ client/Providers/InputSelectProvider.tsx
@@ -35,6 +35,7 @@ export const InputSelectProvider = ( { children }: { children: ReactNode } ) => 
   useEffect( () => {
     getSelectedInputField( setSelectedInput, selectedInputRef );
     getSelectedGutenbergBlock( setSelectedInput );
+    getSelectedElementorBlock( setSelectedInput );
     getSelectedPostTitle( setSelectedInput );
   }, [] );
 
