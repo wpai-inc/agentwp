4b70f30e50ea22561aca56017afc106a9b511a7e
wesleyfantinel <wesley.fantinel@gmail.com>
2024-08-23 10:23:44 -0300

    revert: Updated spinner

--- client/Components/Spinner.tsx
+++ client/Components/Spinner.tsx
@@ -1,26 +1,25 @@
 import { cn } from '@/lib/utils.js';
 
-export function Spinner( { show = true }: { show?: boolean } ) {
+export function Spinner( { show }: { show: boolean } ) {
   return (
     <svg
       className={ cn( 'animate-spin h-5 w-5 text-gray-400 hidden', {
-      className={ cn( 'animate-spin -ml-1 mr-3 h-6 w-6 hidden', {
         'inline-block': show,
       } ) }
-      viewBox="0 0 18 18"
+      xmlns="http://www.w3.org/2000/svg"
       fill="none"
-      xmlns="http://www.w3.org/2000/svg">
+      viewBox="0 0 24 24">
+      <circle
+        className="opacity-0"
+        cx="12"
+        cy="12"
+        r="10"
+        stroke="currentColor"
+        strokeWidth="4"></circle>
       <path
-        d="M9 17C13.4183 17 17 13.4183 17 9C17 4.58172 13.4183 1 9 1C4.58172 1 1 4.58172 1 9C1 13.4183 4.58172 17 9 17Z"
-        stroke="#EBEBEB"
-        fill="transparent"
-      />
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M8.25 1C8.25 0.585786 8.58579 0.25 9 0.25C13.8325 0.25 17.75 4.16751 17.75 9C17.75 9.41421 17.4142 9.75 17 9.75C16.5858 9.75 16.25 9.41421 16.25 9C16.25 4.99594 13.0041 1.75 9 1.75C8.58579 1.75 8.25 1.41421 8.25 1Z"
-        className="fill-brand-primary"
-      />
+        className="opacity-1"
+        fill="currentColor"
+        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
     </svg>
   );
 }
