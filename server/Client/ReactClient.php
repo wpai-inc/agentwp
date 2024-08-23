fb655bd00bb5e03512abd43ea2fbcc75a90457e4
Ovidiu Galatan <galatanovidiu@gmail.com>
2024-08-23 16:29:42 +0300

    Page Data will be managed by ReactClient

--- server/Client/ReactClient.php
+++ server/Client/ReactClient.php
@@ -7,6 +7,7 @@ use WpAi\AgentWp\Contracts\ClientAppInterface;
 use WpAi\AgentWp\Contracts\ClientSetupLocationInterface;
 use WpAi\AgentWp\Contracts\Registrable;
 use WpAi\AgentWp\Main;
+use WpAi\AgentWp\Services\AwpRestRoute;
 
 abstract class ReactClient implements ClientAppInterface, Registrable
 {
@@ -133,6 +134,47 @@ abstract class ReactClient implements ClientAppInterface, Registrable
     public function registerPageProps()
     {
         wp_localize_script($this->slug('-'), $this->slug('_'), $this->pageProps());
+        wp_localize_script($this->slug('-'), 'agentwp_settings', $this->pageData());
+    }
+
+    public function pageData()
+    {
+        $current_user_data = wp_get_current_user()->data;
+        // only keep the necessary data
+        $current_user = [
+            'ID' => $current_user_data->ID,
+            'user_email' => $current_user_data->user_email,
+            'user_login' => $current_user_data->user_login,
+            'user_nicename' => $current_user_data->user_nicename,
+            'display_name' => $current_user_data->display_name,
+            'avatar_url' => get_avatar_url($current_user_data->ID),
+            'roles' => wp_get_current_user()->roles,
+        ];
+
+        return [
+            'home_url' => home_url(),
+            'plugin_url' => $this->main->pluginUrl,
+            'settings_page' => $this->main->settingsPage,
+            'rest_endpoint' => AwpRestRoute::REST_ROUTE_ENDPOINT,
+            'rest_route' => rest_url(),
+            'admin_route' => admin_url(),
+            'nonce' => wp_create_nonce(Main::nonce()),
+            'wp_rest_nonce' => wp_create_nonce('wp_rest'),
+            'is_admin' => $this->main->auth->isAdmin(),
+            'agentwp_manager' => $this->main->auth->isManager(),
+            'agentwp_users_manager' => $this->main->auth->canManageUsers(),
+            'agentwp_access' => $this->main->auth->hasAccess(),
+            'access_token' => $this->main->auth->getAccessToken(),
+            'refresh_token' => $this->main->auth->getRefreshToken(),
+            'site_id' => $this->main->siteId(),
+            'client_id' => $this->main->settings->client_id,
+            'api_host' => $this->main->apiClientHost(),
+            'user' => $current_user,
+            'onboarding_completed' => $this->main->settings->onboarding_completed,
+            'account_settings' => $this->main->accountSettings()->get(),
+            'general_settings' => $this->main->settings->getGeneralSettings(),
+        ];
+
     }
 
     public function registerControllers()
