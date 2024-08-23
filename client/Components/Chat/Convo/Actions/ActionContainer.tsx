b8dd2c199276d36344549899d9ee555e4e658502
wesleyfantinel <wesley.fantinel@gmail.com>
2024-08-23 10:17:09 -0300

    feat: implement expandable

--- client/Components/Chat/Convo/Actions/ActionContainer.tsx
+++ client/Components/Chat/Convo/Actions/ActionContainer.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { cn } from '@/lib/utils';
 import { Spinner } from '@/Components/Spinner';
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from '@/Components/ui/collapsible';
 import IconRenew from '@material-design-icons/svg/outlined/autorenew.svg?react';
+import IconAdd from '@material-design-icons/svg/outlined/add.svg?react';
+import IconRemove from '@material-design-icons/svg/outlined/remove.svg?react';
 
 export default function ActionContainer( {
   title,
@@ -18,11 +21,12 @@ export default function ActionContainer( {
   handleRetry?: () => void;
   icon?: React.ReactNode;
 } ) {
+  const [ isOpen, setIsOpen ] = useState( false );
+
   const iconClassName = 'text-gray-400 h-5 w-5';
 
   function shouldClick() {
     if ( handleRetry ) return true;
-    if ( children ) return true;
     return false;
   }
 
@@ -31,7 +35,9 @@ export default function ActionContainer( {
   }
 
   return (
-    <div
+    <Collapsible
+      open={ isOpen }
+      onOpenChange={ setIsOpen }
       onClick={ handleOnClick }
       className={ cn(
         'min-h-10 bg-brand-gray-25 p-2 px-4 items-center rounded-lg gap-4 my-2 border-2 border-brand-gray-25 text-gray-700 text-sm w-full',
@@ -48,8 +54,14 @@ export default function ActionContainer( {
           <p className="overflow-hidden text-ellipsis whitespace-nowrap">{ title }</p>
         </div>
         { ! pending && handleRetry && <IconRenew className={ iconClassName } /> }
+        { children && ! handleRetry && (
+          <CollapsibleTrigger>
+            { ! isOpen && <IconAdd className={ iconClassName } /> }
+            { isOpen && <IconRemove className={ iconClassName } /> }
+          </CollapsibleTrigger>
+        ) }
       </div>
-      { children }
-    </div>
+      { children && <CollapsibleContent className="pt-4">{ children }</CollapsibleContent> }
+    </Collapsible>
   );
 }
