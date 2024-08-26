5e1e227462a5a82368b6ebb4d11a7eaa80348f39
Greg Hunt <freshbrewedweb@gmail.com>
2024-08-26 12:23:56 -0400

    Fixed error states on actions and the infinite loading error

--- client/Components/Chat/Convo/Actions/ActionContainer.tsx
+++ client/Components/Chat/Convo/Actions/ActionContainer.tsx
@@ -5,6 +5,8 @@ import { Collapsible, CollapsibleContent, CollapsibleTrigger } from '@/Component
 import IconRenew from '@material-design-icons/svg/outlined/autorenew.svg?react';
 import IconAdd from '@material-design-icons/svg/outlined/add.svg?react';
 import IconRemove from '@material-design-icons/svg/outlined/remove.svg?react';
+import IconAlert from '@material-design-icons/svg/outlined/error_outline.svg?react';
+import { AgentTooltip } from '@/Components/ui/tooltip';
 
 export default function ActionContainer( {
   title,
@@ -13,6 +15,7 @@ export default function ActionContainer( {
   className,
   handleRetry,
   icon,
+  error,
 }: {
   title: string;
   pending: boolean;
@@ -20,10 +23,12 @@ export default function ActionContainer( {
   className?: string;
   handleRetry?: () => void;
   icon?: React.ReactNode;
+  error?: string;
 } ) {
   const [ isOpen, setIsOpen ] = useState( false );
+  const hasError = error && error.length > 0;
 
-  const iconClassName = 'text-gray-400 h-5 w-5';
+  const iconClassName = 'opacity-40 h-5 w-5';
 
   function shouldClick() {
     if ( handleRetry ) return true;
@@ -34,20 +39,30 @@ export default function ActionContainer( {
     if ( handleRetry ) return handleRetry();
   }
 
+  console.log( error );
   return (
     <Collapsible
       open={ isOpen }
       onOpenChange={ setIsOpen }
       onClick={ handleOnClick }
       className={ cn(
-        'min-h-10 bg-brand-gray-25 p-2 px-4 items-center rounded-lg gap-4 my-2 border-2 border-brand-gray-25 text-gray-700 text-sm w-full',
-        { 'cursor-pointer': shouldClick() },
+        'min-h-10 bg-brand-gray-25 p-2 px-4 items-center rounded-lg gap-4 my-2 text-gray-700 text-sm w-full',
+        {
+          'cursor-pointer': shouldClick(),
+          'opacity-60 text-gray-500': hasError,
+        },
         className,
       ) }>
       <div className="flex justify-between items-center">
         <div className="flex gap-4 items-center max-w-full">
           { pending && ! handleRetry ? (
             <Spinner show={ true } />
+          ) : hasError ? (
+            <AgentTooltip content={ error }>
+              <button>
+                <IconAlert className={ iconClassName } />
+              </button>
+            </AgentTooltip>
           ) : (
             icon && React.cloneElement( icon, { className: iconClassName } )
           ) }
