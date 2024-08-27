b356cde6127e1b545efd7e2d5266abbe762f6401
Ovidiu Galatan <galatanovidiu@gmail.com>
2024-08-27 18:06:56 +0300

    Updated comment

--- server/Page/Admin/Chat.php
+++ server/Page/Admin/Chat.php
@@ -7,7 +7,7 @@ use WpAi\AgentWp\Client\ReactClient;
 class Chat extends ReactClient
 {
     /**
-     * Order matters, earlier locations take precedence.
+     * Order matters, later locations take precedence.
      */
     protected array $locations = [
         \WpAi\AgentWp\Client\Locations\CoreChat::class,
