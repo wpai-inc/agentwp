f7827e85d13d0e56a6ebe033990c247814e71df0
Ovidiu Galatan <galatanovidiu@gmail.com>
2024-09-02 15:50:33 +0300

    Update footer style in HasPage trait

--- server/Traits/HasPage.php
+++ server/Traits/HasPage.php
@@ -23,7 +23,7 @@ trait HasPage
     public function updateFooter(): string
     {
         return <<<HTML
-            <div style="float: right;">
+            <div style="float: right; padding-left: 12px">
             Made with love ❤️ by the <a href="{$this->main->attributionUrl}" target="_blank">{$this->main->companyName}</a>
             </div>
         HTML;
