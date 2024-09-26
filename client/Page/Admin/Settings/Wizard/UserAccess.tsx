ba5e1443614ad18d731d9810e2bbfb57d9af0dae
Greg Hunt <freshbrewedweb@gmail.com>
2024-09-26 17:49:05 -0400

    Cleaned up wizard components

--- client/Page/Admin/Settings/Wizard/UserAccess.tsx
+++ client/Page/Admin/Settings/Wizard/UserAccess.tsx
@@ -1,20 +1,16 @@
-import Logo from '@/Components/Logo';
 import { Button } from '@/Components/ui/button';
 import UsersManagement from '@/Page/Admin/Settings/SubPages/UsersManagement';
+import WizardHeader from '../Partials/WizardHeader';
+import WizardContainer from '../Partials/WizardContainer';
 
 export default function UserAccess( { onGoToAboutPage }: { onGoToAboutPage: () => void } ) {
   return (
-    <div className="max-w-[470px] mx-auto text-base mt-12">
-      <Logo className="mx-auto w-14 h-14" />
-      <div className="text-4xl font-bold text-center mt-4 leading-10">
-        Choose Who Can Use AgentWP
-      </div>
+    <WizardContainer className="space-y-4">
+      <WizardHeader>Choose Who Can Use AgentWP</WizardHeader>
       <UsersManagement />
-      <div className={ 'text-center mt-16' }>
-        <Button variant="brand" className="px-20" onClick={ () => onGoToAboutPage() }>
-          Continue
-        </Button>
-      </div>
-    </div>
+      <Button variant="brand" size="lg" className="w-full" onClick={ () => onGoToAboutPage() }>
+        Continue
+      </Button>
+    </WizardContainer>
   );
 }
