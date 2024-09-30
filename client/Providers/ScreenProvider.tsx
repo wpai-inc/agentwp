729414b73b7c459421bde9cdfe30c808331a554b
Ovidiu Galatan <galatanovidiu@gmail.com>
2024-09-30 21:56:49 +0300

    Remove email and password fields from the screenshot
    Also remove the avatar and the site-icon-preview which cause errors

--- client/Providers/ScreenProvider.tsx
+++ client/Providers/ScreenProvider.tsx
@@ -43,11 +43,23 @@ async function getScreenshot(): Promise< string > {
   const node = document.body;
 
   if ( node ) {
-    const filter = ( node: HTMLElement ) => {
-      return node.id !== 'agentwp-admin-chat';
+    const filter = ( node: Node ): boolean => {
+      if ( node instanceof HTMLElement ) {
+        return (
+          node.id !== 'agentwp-admin-chat' &&
+          node.id !== 'site-icon-preview' &&
+          ! node.classList.contains( 'avatar' ) &&
+          ! ( node instanceof HTMLInputElement && node.type === 'email' ) &&
+          ! ( node instanceof HTMLInputElement && node.type === 'password' )
+        );
+      }
+      return true;
     };
 
-    return await toJpeg( node, { quality: 0.5, filter } );
+    return await toJpeg( node, {
+      quality: 0.5,
+      filter,
+    } );
   } else {
     console.error( 'Problemo.' );
     return '';
@@ -68,8 +80,12 @@ export default function ScreenProvider( { children }: { children: React.ReactNod
       const url = window.location.href;
       const title = document.title;
       const links = Array.from( document.links ).map( link => link.href );
-      const screenshot = getAccountSetting( 'visionEnabled' ) ? await getScreenshot() : '';
-
+      const screenshot = getAccountSetting( 'visionEnabled' )
+        ? await getScreenshot().catch( err => {
+            console.error( 'Error getting screenshot:', err );
+            return '';
+          } )
+        : '';
       setScreen( {
         url,
         title,
