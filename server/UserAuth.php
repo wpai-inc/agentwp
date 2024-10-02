dc6a18a30f5cc7de93539d66ff15d9e8e07657c7
Ovidiu Galatan <galatanovidiu@gmail.com>
2024-10-02 20:01:23 +0300

    Fix verification key sanitization in UserAuth.php

--- server/UserAuth.php
+++ server/UserAuth.php
@@ -33,7 +33,7 @@ class UserAuth
 
     public function hasValidVerificationKey(): bool
     {
-        $verification_key = isset($_GET['verification_key']) ? sanitize_text_field($_GET['verification_key']) : '';
+        $verification_key = isset($_GET['verification_key']) ? sanitize_text_field(wp_unslash($_GET['verification_key'])) : '';
 
         if (empty($verification_key)) {
             return false;
