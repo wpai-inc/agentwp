860e504e9ff69e728322df6b568c274647473e2d
Ovidiu Galatan <galatanovidiu@gmail.com>
2024-10-03 20:36:31 +0300

    Add .distignore and wp-dist-archive script

---
+++ bin/wp-dist-archive.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# run composer in production mode
+composer install --no-dev --prefer-dist --optimize-autoloader
+npm install
+npm run build
+
+# create the dist archive
+echo "Creating dist archive..."
+wp --allow-root dist-archive .
+
+# get the file of the dist archive
+
+echo "Dist archive created: $PLUGIN_FILE"
+
+# move agentwp.*.zip inside plugin-builds folder
+CURRENT_DIR=$(pwd)
+PLUGIN_FILE=$(find "$CURRENT_DIR/.." -name 'agentwp.*.zip' -print -quit)
+
+if [ -n "$PLUGIN_FILE" ]; then
+    PLUGIN_FILENAME=$(basename "$PLUGIN_FILE")
+    BUILDS_FOLDER="${CURRENT_DIR}/plugin-builds"
+
+    if [ ! -d "$BUILDS_FOLDER" ]; then
+        mkdir "$BUILDS_FOLDER"
+    fi
+
+    cp "$PLUGIN_FILE" "$BUILDS_FOLDER/main.zip"
+
+    if [ -f "$BUILDS_FOLDER/$PLUGIN_FILENAME" ]; then
+        rm "$BUILDS_FOLDER/$PLUGIN_FILENAME"
+    fi
+    mv "$PLUGIN_FILE" "$BUILDS_FOLDER/"
+fi
+
+ls -ll "${CURRENT_DIR}/plugin-builds"
