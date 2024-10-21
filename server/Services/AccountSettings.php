f8e3def683ebad9f38e0b510f374881b5a330e8a
Ovidiu Galatan <galatanovidiu@gmail.com>
2024-10-21 22:57:44 +0300

    Add caching for site settings retrieval
    Introduced transient caching in the `get` method of `AccountSettings` to minimize repetitive API calls. This implementation uses a 60-second expiration to store and retrieve site settings efficiently.

--- server/Services/AccountSettings.php
+++ server/Services/AccountSettings.php
@@ -8,17 +8,20 @@ class AccountSettings
 {
     private WpAwpClient $client;
 
+    private int $site_settings_expiration = 60;
+
     public function __construct(WpAwpClient $client)
     {
         $this->client = $client;
     }
 
-    /**
-     * @return array|\WP_Error
-     */
-    public function get()
+    public function get(): \WP_Error|array
     {
+        if (false !== ($res = get_transient('site_settings'))) {
+            return $res;
+        }
         $res = $this->client->siteSettingAll();
+        set_transient('site_settings', $res, $this->site_settings_expiration);
 
         return is_array($res) ? $res : [];
     }
