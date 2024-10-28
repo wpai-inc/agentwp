1e48ed181a6653899f84851ad1422e5a3fbe7960
Ovidiu Galatan <galatanovidiu@gmail.com>
2024-10-28 17:04:15 +0200

    Use builtin error

--- server/Http/Middleware/CheckRestRequestNonce.php
+++ server/Http/Middleware/CheckRestRequestNonce.php
@@ -9,11 +9,8 @@ class CheckRestRequestNonce extends Middleware
     public function handle(WP_REST_Request $request)
     {
         $nonce = $request->get_header('x-wp-nonce');
-        if (! $nonce) {
-            return new \WP_Error('rest_forbidden', 'Nonce is required', ['status' => 403]);
-        }
-        if (! wp_verify_nonce($nonce, 'wp_rest')) {
-            return new \WP_Error('rest_forbidden', 'Invalid nonce', ['status' => 403]);
+        if (empty($nonce) || ! wp_verify_nonce($nonce, 'wp_rest')) {
+            return $this->error('invalid_nonce');
         }
     }
 }
