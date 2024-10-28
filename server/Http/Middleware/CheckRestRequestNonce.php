1e32d57b27c78fd2bc4a55e22e51aceddb4a46d1
Ovidiu Galatan <galatanovidiu@gmail.com>
2024-10-28 20:13:08 +0200

    Update nonce verification in CheckRestRequestNonce middleware
    Previously, the middleware only retrieved the nonce from the 'x-wp-nonce' header. This update allows it to also check the '_wpnonce' request parameter, enhancing flexibility in how nonces are provided.

--- server/Http/Middleware/CheckRestRequestNonce.php
+++ server/Http/Middleware/CheckRestRequestNonce.php
@@ -8,7 +8,7 @@ class CheckRestRequestNonce extends Middleware
 {
     public function handle(WP_REST_Request $request)
     {
-        $nonce = $request->get_header('x-wp-nonce');
+        $nonce = $request->get_header('x-wp-nonce') ?: $request->get_param('_wpnonce');
         if (empty($nonce) || ! wp_verify_nonce($nonce, 'wp_rest')) {
             return $this->error('invalid_nonce');
         }
