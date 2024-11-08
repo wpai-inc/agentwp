1434b0feab756f548d6682e909f7a702fd2a987f
Greg Hunt <freshbrewedweb@gmail.com>
2024-11-08 14:12:28 -0500

    Added UI for abilities control

--- client/Page/Admin/Settings/Partials/ChatSettings.tsx
+++ client/Page/Admin/Settings/Partials/ChatSettings.tsx
@@ -4,6 +4,9 @@ import { usePage } from '@/Providers/PageProvider';
 import { useRestRequest } from '@/Providers/RestRequestProvider';
 import { optimistic } from '@/lib/utils';
 import { DataListItem } from '@/Components/ui/dl';
+
+const abilities = [ 'navigate', 'query' ];
+
 export default function ChatSettings() {
   const {
     page: { account_settings },
@@ -11,6 +14,7 @@ export default function ChatSettings() {
 
   const [ settings, setSettings ] = useState< App.Data.SiteSettingData[] >( account_settings );
   const { proxyApiRequest } = useRestRequest();
+  const isBoolean = ( value: any ) => typeof value === 'boolean';
 
   async function handleChange( name: App.Enums.SiteSettingValue, checked: boolean ) {
     const prevSettings = settings;
@@ -23,6 +27,7 @@ export default function ChatSettings() {
       name,
       value: checked,
       label: null,
+      canUpdate: true,
     };
 
     optimistic(
@@ -37,12 +42,39 @@ export default function ChatSettings() {
       { settings.map( setting => (
         <DataListItem
           key={ setting.name }
-          label={ <label className="font-bold">{ setting.label }</label> }>
-          <Switch
-            id={ setting.name }
-            checked={ setting.value }
-            onCheckedChange={ ( checked: boolean ) => handleChange( setting.name, checked ) }
-          />
+          label={
+            <div>
+              <label className="font-bold">{ setting.label }</label>
+              { setting.name === 'abilities' && (
+                <p className="text-sm">Abilities are the actions AgentWP can take on your site.</p>
+              ) }
+            </div>
+          }>
+          { setting.name === 'abilities' ? (
+            <div className="space-y-2">
+              { abilities.map( ( ability: string ) => (
+                <label key={ ability } className="capitalize flex gap-2 items-center">
+                  <input
+                    type="checkbox"
+                    checked={ setting.value.includes( ability ) }
+                    onChange={ ( e: React.ChangeEvent< HTMLInputElement > ) => {
+                      const newItems = e.target.checked
+                        ? [ ...setting.value, ability ]
+                        : setting.value.filter( ( item: string ) => item !== ability );
+                      handleChange( setting.name, newItems );
+                    } }
+                  />
+                  { ability }
+                </label>
+              ) ) }
+            </div>
+          ) : isBoolean( setting.value ) ? (
+            <Switch
+              id={ setting.name }
+              checked={ setting.value }
+              onCheckedChange={ ( checked: boolean ) => handleChange( setting.name, checked ) }
+            />
+          ) : null }
         </DataListItem>
       ) ) }
     </>
