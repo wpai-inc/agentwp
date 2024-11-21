ebbf910dce754005ebc75fcedf43e81368d8bf78
Ovidiu Galatan <galatanovidiu@gmail.com>
2024-11-21 20:30:10 +0200

    Add a check for unexecuted actions to display the thinking component

--- client/Components/Chat/Convo/Message/AgentResponse.tsx
+++ client/Components/Chat/Convo/Message/AgentResponse.tsx
@@ -45,6 +45,10 @@ export default function AgentResponse( {
       aa => ! [ 'message', 'navigation_confirmation' ].includes( aa.action?.ability ),
     ) ?? [];
 
+  const hasUnexecutedActions = otherActions.some(
+    action => ! action.hasExecuted && ! action.hasError,
+  );
+
   const incomplete = agentActions?.length === 0;
 
   const queryActions = agentActions?.filter( aa => aa.action?.ability === 'query' ) ?? [];
@@ -54,7 +58,7 @@ export default function AgentResponse( {
   return (
     <div className="text-black/60">
       { otherActions.length > 0 ? (
-        <div className="mb-4 flex-1 space-y-2">
+        <div className="flex-1 mb-4 space-y-2">
           { otherActions.map( aa => {
             if ( aa.action ) {
               return <ActionComponent key={ aa.id } { ...aa } />;
@@ -68,7 +72,7 @@ export default function AgentResponse( {
           name="AgentWP"
           time={ time }
           image={ logoUrl }
-          className="border-brand border p-1"
+          className="p-1 border border-brand"
         />
         <div className="flex items-center gap-4">
           { ! incomplete && <Rate /> }
@@ -89,7 +93,7 @@ export default function AgentResponse( {
               <DropdownMenuItem>
                 <span className="flex gap-2">
                   Message ID:{ ' ' }
-                  <strong className="inline-block max-w-24 truncate font-mono text-sm font-semibold">
+                  <strong className="inline-block font-mono text-sm font-semibold truncate max-w-24">
                     <Copyable text={ userRequestId } />
                   </strong>
                 </span>
@@ -130,7 +134,7 @@ export default function AgentResponse( {
       ) : (
         <>
           { aborted && <ActionAborted /> }
-          { ! aborted && pending && <ActionPending /> }
+          { ! aborted && ( pending || hasUnexecutedActions ) && <ActionPending /> }
           { ! aborted && incomplete && ! pending && (
             <>
               { queryActions.length > 0 ? (
