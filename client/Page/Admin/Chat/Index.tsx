c78b89abd7b652842466b9540f4c4ce6b77482d1
Greg Hunt <freshbrewedweb@gmail.com>
2024-11-22 09:59:50 -0500

    Pass the shadowRoot as ref to components

--- client/Page/Admin/Chat/Index.tsx
+++ client/Page/Admin/Chat/Index.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import type { PageData } from '@/Types/types';
 import { AWPRootType } from '@/Types/types';
 import ChatApp from '@/Components/Chat';
 import { NotificationsProvider } from '@/Providers/NotificationProvider';
 import { PageProvider } from '@/Providers/PageProvider';
-import root from 'react-shadow';
 import styles from '@/assets/styles/inline-app.css?inline';
 
 declare global {
@@ -16,21 +15,19 @@ declare global {
 
 declare const agentwpData: PageData;
 
-const rootElement = document.getElementById( 'agentwp-admin-chat' );
+const rootElement = document.getElementById( 'agentwp-admin-chat' ) as AWPRootType;
 if ( rootElement ) {
-  window.agentwp = rootElement as AWPRootType;
-  const theRootElement = ReactDOM.createRoot( rootElement );
-  theRootElement.render(
-    <root.div id="agentwp-chat-shadow-root">
-      <React.StrictMode>
-        <NotificationsProvider>
-          <PageProvider page={ agentwpData }>
-            <ChatApp />
-            <style type="text/css">{ styles }</style>
-          </PageProvider>
-        </NotificationsProvider>
-      </React.StrictMode>
-    </root.div>,
+  window.agentwp = rootElement;
+  const reactRoot = ReactDOM.createRoot( rootElement );
+  reactRoot.render(
+    <StrictMode>
+      <NotificationsProvider>
+        <PageProvider page={ agentwpData }>
+          <ChatApp />
+          <style type="text/css">{ styles }</style>
+        </PageProvider>
+      </NotificationsProvider>
+    </StrictMode>,
   );
 } else {
   // Handle the case where the root element is not found
